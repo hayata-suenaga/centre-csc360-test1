492889868003af13fd11b99641e2f0a1853adf6c
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-02-08 11:09:31 -0500

    Implement Employee class

---
+++ src/main/java/Employees.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class Employees {
+    final private List<Employee> employees = new ArrayList<>();
+
+    public void addEmployee(Employee newEmployee) {
+        employees.add(newEmployee);
+    }
+
+    public void setHoursForEmployee(String name, int newHours) throws NoSuchElementException {
+        Employee employee = findEmployeeByName(name);
+        if (employee == null)
+            throw new NoSuchElementException("Employee with the name " + name + " does not exist");
+        employee.setHours(newHours);
+    }
+
+    public List<Double> payAll() {
+        List<Double> pays = new ArrayList<>();
+        for (Employee employee : employees) {
+            pays.add(employee.pay());
+        }
+        return pays;
+    }
+
+    private Employee findEmployeeByName(String name) {
+        for (Employee employee : employees) {
+            if (employee.name.equals(name)) {
+                return employee;
+            }
+        }
+        return null;
+    }
+}
